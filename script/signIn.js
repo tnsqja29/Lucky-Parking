f93e421fe60c45cd0166a6beb1875b226bf270d2
tnsqja29 <87375722+tnsqja29@users.noreply.github.com>
2024-09-11 09:52:26 +0900

    main

--- script/signIn.js
+++ script/signIn.js
@@ -1,13 +1,13 @@
 $(function() {
-    // 1. 카카오 SDK 초기화 (JavaScript 키 입력)
-         Kakao.init('YOUR_KAKAO_JAVASCRIPT_KEY'); // YOUR_KAKAO_JAVASCRIPT_KEY를 발급받은 JavaScript 키로 대체하세요.
- 
-         // 2. 카카오톡 로그인 버튼 클릭 시 팝업 창 열기
-         document.getElementById('kakao-login-btn').addEventListener('click', function(event) {
-             event.preventDefault();
- 
-             const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=YOUR_KAKAO_JAVASCRIPT_KEY&redirect_uri=YOUR_KAKAO_REDIRECT_URI&response_type=code`;
- 
-             window.open(kakaoLoginUrl, 'kakaoLogin', 'width=500,height=700');
-         });
- })
+   // 1. 카카오 SDK 초기화 (JavaScript 키 입력)
+        Kakao.init('YOUR_KAKAO_JAVASCRIPT_KEY'); // YOUR_KAKAO_JAVASCRIPT_KEY를 발급받은 JavaScript 키로 대체하세요.
+
+        // 2. 카카오톡 로그인 버튼 클릭 시 팝업 창 열기
+        document.getElementById('kakao-login-btn').addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=YOUR_KAKAO_JAVASCRIPT_KEY&redirect_uri=YOUR_KAKAO_REDIRECT_URI&response_type=code`;
+
+            window.open(kakaoLoginUrl, 'kakaoLogin', 'width=500,height=700');
+        });
+})
